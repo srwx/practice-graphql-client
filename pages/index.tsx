e62115be354a53e168175f382234f04211110751
Sorawong L <Sorawong2543@gmail.com>
2022-12-14 23:08:19 +0700

    fetch data via gql in ssg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,34 @@
-export default function Home() {
-  return <div className="text-red-600">Hello World</div>
+import { gql } from "@apollo/client"
+import { client } from "graphql/apollo-client"
+import { GetStaticProps } from "next"
+
+export default function Home({ books }: { books: any }) {
+  return (
+    <>
+      {books.map((book: any) => (
+        <div key={book.id}>
+          {book.id}. {book.name}
+        </div>
+      ))}
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await client.query({
+    query: gql`
+      query Books {
+        books {
+          id
+          name
+        }
+      }
+    `,
+  })
+
+  return {
+    props: {
+      books: data.books,
+    },
+  }
 }
