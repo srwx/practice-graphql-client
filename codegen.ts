0c1efbe331852df79f5bf791e940c034f2fe5106
Sorawong L <Sorawong2543@gmail.com>
2022-12-15 00:05:41 +0700

    init `codegen.ts`

---
+++ codegen.ts
@@ -0,0 +1,15 @@
+import { CodegenConfig } from "@graphql-codegen/cli"
+
+const config: CodegenConfig = {
+  schema: "http://localhost:4000/graphql", // graphql server url
+  documents: "./graphql/gql/**/.gql", // path to my .graphql that used for query & mutation
+  generates: {
+    // key represents an output path for the generated code ("./graphql/codegen/")
+    // value represents options for the generated code
+    "./graphql/codegen/": {
+      preset: "client",
+      plugins: [],
+    },
+  },
+  overwrite: true, // overwrite files if they already exist when generating code
+}
